7d46081ac9bade021e23f84e512c2b9921672893
Andrew Ho <aqho@users-MacBook-Pro.local>
2019-04-16 14:34:36 -0700

    Initial Commit

---
+++ Detect_Polygon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Detect_Polygon
+//
+//  Created by Andrew Ho on 4/16/19.
+//  Copyright © 2019 Andrew Ho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
